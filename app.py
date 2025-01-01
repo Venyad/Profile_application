3b181ec460c86234ecb1c19580524baa1c02f9fb
Venya <venyadurgam25@gmail.com>
2025-01-01 14:53:28 +0530

    Updated user registration functionality in app.py

--- app.py
+++ app.py
@@ -45,4 +45,31 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/register', methods = ['GET','POST'])
-
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form and 'city' in request.form and 'country' in request.form and 'postalcode' in request.form and 'organisation' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        organisation = request.form['organisation']
+        address = request.form['address']
+        city = request.form['city']
+        state = request.form['state']
+        country = request.form['country']
+        postalcode = request.form['postalcode']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = % s' , (username,)
+        )
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers' 
+        else:
+            cursor.execute('INSERT INTO accounts VALUES \ (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s)',
+                           (username, password, email, 
+                            organisation, address, city,
+                            state, country, postalcode, ))
